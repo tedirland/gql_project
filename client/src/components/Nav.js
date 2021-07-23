6142dca28e1ff4569346dcd1f0a7d37ceb771322
Ted Irland <irlandth@gmail.com>
2021-07-23 11:32:02 -0500

    Implemented logout using firebase functionality

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
+import { AuthContext } from '../context/authContext';
 
 const Nav = () => {
+  const { state, dispatch } = useContext(AuthContext);
+  let history = useHistory();
+
+  const { user } = state;
+
+  const logout = () => {
+    auth.signOut();
+    dispatch({
+      type: 'LOGGED_IN_USER',
+      payload: null,
+    });
+
+    history.push('/login');
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -31,6 +48,13 @@ const Nav = () => {
               Register
             </Link>
           </li>
+          {user && (
+            <li className="nav-item">
+              <a onClick={logout} className="nav-link nav-item" href="/login">
+                Logout
+              </a>
+            </li>
+          )}
         </ul>
         <div className="ml-auto"></div>
       </div>
--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,4 +1,5 @@
-import React, { useReducer, createContext } from 'react';
+import React, { useReducer, createContext, useEffect } from 'react';
+import { auth } from '../firebase';
 
 //reducer func will update state
 const firebaseReducer = (state, action) => {
@@ -23,6 +24,26 @@ const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(firebaseReducer, initialState);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async user => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: { email: user.email, token: idTokenResult.token },
+        });
+      } else {
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: null,
+        });
+      }
+    });
+    //cleanup
+    return () => unsubscribe();
+  }, []);
 
   const value = { state, dispatch };
 
