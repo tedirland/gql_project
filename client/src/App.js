75369ef5f55bf92aa395c8938262d535c86eab19
Ted Irland <irlandth@gmail.com>
2021-07-24 08:51:10 -0500

    Created dummy authcheck function

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,24 @@ import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import CompleteRegistration from './pages/auth/CompleteRegistration';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache(),
-});
+import { useContext } from 'react';
+import { AuthContext } from './context/authContext';
 
 const App = () => {
+  const { state } = useContext(AuthContext);
+  const { user } = state;
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:8000/graphql',
+    cache: new InMemoryCache(),
+    request: operation => {
+      operation.setContext({
+        headers: {
+          authtoken: user ? user.token : '',
+        },
+      });
+    },
+  });
   return (
     <ApolloProvider client={client}>
       <Nav />
